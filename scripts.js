3609da310f872d0e6fa0a4234856edede08ae417
haiseno <haise.zhao@gmail.com>
2023-09-09 21:46:51 -0700

    Add function to play a round of rps
    If the player choice is the same as computer choice, it's a tie
    and we are done. Otherwise, we enter the switch statement to check
    for each case.
    
    Within the switch statement, I did not explicitly write conditionals
    for the "lose" outcome because if the player does not win against
    the computer, the only other outcome is "lose"; if it were a tie, we
    would have never even entered the switch statement.

--- scripts.js
+++ scripts.js
@@ -8,3 +8,27 @@ function getComputerChoice() {
       return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === computerSelection) {
+    return "It's a tie!";
+  }
+  
+  switch (playerSelection.toLowerCase()) {
+    case "rock":
+      if (computerSelection === "scissors") {
+        return "You win! Rock beats scissors.";
+      }
+      return "You lose! Paper beats rock.";
+    case "paper":
+      if (computerSelection === "rock") {
+        return "You win! Paper beats rock.";
+      }
+      return "You lose! Scissors beat paper.";
+    case "scissors":
+      if (computerSelection === "paper") {
+        return "You win! Scissors beat paper.";
+      }
+      return "You lose! Rock beats scissors.";
+  }
+}
